8d76f3ff756babc0ac14fce62bc28804ea6aa737
Zeldariam <moez.uzumaki@gmail.com>
2019-11-13 08:58:45 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeubleComponent } from './meuble/meuble.component';
+import { MeublesListComponent } from './meubles-list/meubles-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeubleComponent,
+    MeublesListComponent
   ],
   imports: [
     BrowserModule
